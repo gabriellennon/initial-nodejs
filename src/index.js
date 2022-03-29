5bb9a7cbe8d5c6f188ba3f1a5f45022fd7ae27c7
frontend <gabriellennon@wizsolucoes.com.br>
2022-03-28 23:07:15 -0300

    type of params

--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    //enviando uma mensagem para quem ta fazendo a request
-    // return response.send("Hello world!");
+app.use(express.json());
 
-    return response.json({ message: "Hello World - Fundamentos Node JS!"});
+// app.get("/", (request, response) => {
+//     enviando uma mensagem para quem ta fazendo a request
+//     return response.send("Hello world!");
+
+//     return response.json({ message: "Hello World - Fundamentos Node JS!"});
+// })
+
+app.get('/courses', (request, response) => {
+    const query = request.query;
+    console.log('Query Params =>',query);
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log('Body ->', body)
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put('/courses/:id', (request, response) => {
+    const params = request.params;
+    console.log(params);
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch('/courses/:id', (request, response) => {
+    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete('/courses/:id', (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 4"]);
 })
 
 //Colando a porta
